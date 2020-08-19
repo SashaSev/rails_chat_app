ca812a3170f9fa2efcedde7f7c1203479e0d1c32
svack_test <www.sashsvardanyan1995@gmail.com>
2020-08-19 18:53:59 +0400

    WIP

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  devise_for :admins
+  resources :sessions, only: %i[create destroy]
+  root to: 'home#index'
+
+  mount ActionCable.server => '/cable'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
